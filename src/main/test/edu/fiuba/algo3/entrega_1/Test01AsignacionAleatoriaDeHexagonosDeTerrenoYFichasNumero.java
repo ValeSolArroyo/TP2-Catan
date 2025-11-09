6b5b8cafc5ad9ca8f331645255825fb786f959e2
Chiara <chiara1991.cl@gmail.com>
2025-11-09 15:56:51 -0300

    Borro archivos viejos y pasan los tests.

--- src/main/test/edu/fiuba/algo3/entrega_1/Test01AsignacionAleatoriaDeHexagonosDeTerrenoYFichasNumero.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/Test01AsignacionAleatoriaDeHexagonosDeTerrenoYFichasNumero.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class Test01AsignacionAleatoriaDeHexagonosDeTerrenoYFichasNumero() {
+public class Test01AsignacionAleatoriaDeHexagonosDeTerrenoYFichasNumero {
 
     @Test
     public void TableroTest() {
