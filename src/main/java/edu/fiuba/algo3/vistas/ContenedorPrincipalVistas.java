d0ee5b2b1a7b094d6d2c014639e13557bf115fe7
Rafaela Arias <ariasrafy@gmail.com>
2025-11-29 15:12:46 -0300

    Empezamos a crear nuestros propios componentes - Solo Menú por ahora

--- src/main/java/edu/fiuba/algo3/vistas/ContenedorPrincipalVistas.java
+++ src/main/java/edu/fiuba/algo3/vistas/ContenedorPrincipalVistas.java
@@ -1,6 +1,7 @@
 package edu.fiuba.algo3.vistas;
 
 import edu.fiuba.algo3.controllers.BotonAyudaControlador;
+import edu.fiuba.algo3.vistas.componentes.MenuGlobal;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -13,6 +14,7 @@ public class ContenedorPrincipalVistas extends StackPane {
 
     private Stage stage;
     private BorderPane contenedor;
+    private VBox menuGlobal;
 
     public ContenedorPrincipalVistas(Stage stage) {
         this.stage = stage;
@@ -20,7 +22,7 @@ public class ContenedorPrincipalVistas extends StackPane {
         contenedor = new BorderPane();
         this.getChildren().add(contenedor);
 
-        VBox menuGlobal = crearMenuGlobal();
+        menuGlobal = new MenuGlobal(this).getMenu();
         StackPane.setAlignment(menuGlobal, Pos.BOTTOM_RIGHT);
         StackPane.setMargin(menuGlobal, new Insets(20));
 
@@ -37,62 +39,4 @@ public class ContenedorPrincipalVistas extends StackPane {
     }
 
     public Node getContenido() {return contenedor.getCenter();}
-
-    private VBox crearMenuGlobal() {
-        Button botonMenu = new Button("Menu");
-        botonMenu.getStyleClass().add("boton-menu");
-        botonMenu.setTranslateY(50);
-        botonMenu.setPrefWidth(100);
-        botonMenu.setPrefHeight(25);
-
-        HBox menuDesplegable = new HBox(10);
-        menuDesplegable.setAlignment(Pos.CENTER_RIGHT);
-        menuDesplegable.setVisible(false);
-
-        Button botonMusica = new Button("Musica");
-        botonMusica.getStyleClass().add("boton-menu");
-        botonMusica.setPrefWidth(90);
-        botonMusica.setPrefHeight(28);
-
-        Button botonOpciones = new Button("Opciones");
-        botonOpciones.getStyleClass().add("boton-menu");
-        botonOpciones.setPrefWidth(90);
-        botonOpciones.setPrefHeight(28);
-
-        Button botonAyuda = new Button("Ayuda");
-        botonAyuda.getStyleClass().add("boton-menu");
-        botonAyuda.setPrefWidth(90);
-        botonAyuda.setOnAction(new BotonAyudaControlador(this));
-
-        Button botonAcerca = new Button("Acerca de");
-        botonAcerca.getStyleClass().add("boton-menu");
-        botonAcerca.setPrefWidth(100);
-        botonAcerca.setOnAction(e -> {
-            Alert popup = new Alert(Alert.AlertType.INFORMATION);
-            popup.setTitle("Acerca de");
-            popup.setHeaderText("Catán - Trabajo Práctico de Paradigmas de Programación");
-            popup.setContentText("Facultad de Ingeniería de la UBA, Cátedra Suarez. 2C2025.\n\n" +
-                    "Integrantes:\n★ Arias, Rafaela Pilar, 112272.\n★ Arroyo, Valentina Sol, 112294." +
-                    "\n★ Baéz, Brenda Carolina, 112496.\n★ López Angelini, Chiara, 111225." +
-                    "\n★ Marchesini, Carlota, 112099.");
-            popup.showAndWait();
-        });
-
-        menuDesplegable.getChildren().addAll(botonMusica, botonOpciones, botonAyuda, botonAcerca);
-
-        botonMenu.setOnAction(e -> {
-            boolean estabaAbierto = menuDesplegable.isVisible();
-            menuDesplegable.setVisible(!estabaAbierto);
-            if (!estabaAbierto) {
-                botonMenu.setTranslateY(-15);
-            } else {
-                botonMenu.setTranslateY(50);
-            }
-        });
-
-        VBox contenidoMenu = new VBox(5, botonMenu, menuDesplegable);
-        contenidoMenu.setAlignment(Pos.BOTTOM_RIGHT);
-
-        return contenidoMenu;
-    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.fiuba.algo3 {
     requires javafx.controls;
     requires json.simple;
+    requires javafx.graphics;
     //requires edu.fiuba.algo3;
 
     exports edu.fiuba.algo3;
