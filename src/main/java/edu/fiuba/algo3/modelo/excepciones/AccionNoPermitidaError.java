7a87ff1befccad698d50ad1a48657b88dae31161
Valen <vsarroyo@fi.uba.ar>
2025-12-05 16:34:51 -0300

    arreglo test + diagramas de clase (faltan)

--- src/main/java/edu/fiuba/algo3/modelo/excepciones/AccionNoPermitidaError.java
+++
@@ -1,7 +0,0 @@
-package edu.fiuba.algo3.modelo.excepciones;
-
-public class AccionNoPermitidaError extends RuntimeException {
-    public AccionNoPermitidaError(String message) {
-        super(message);
-    }
-}
